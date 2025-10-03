763f751061cf9485cf5aa44f44d444ebaf7fda25
pranayrauni <ipranaysingh@gmail.com>
2025-10-03 18:23:57 +0530

    update user profile request

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,9 +1,12 @@
+import { useUpdateMyUser } from "@/api/MyUserApi"
 import UserProfileForm from "@/forms/user-profile-form/UseProfileForm"
 
 
 const UserProfilePage = () => {
+    const {updateUser, isPending} = useUpdateMyUser(); 
+
   return (
-    <UserProfileForm/>
+    <UserProfileForm onSave={updateUser} isLoading={isPending}/>
   )
 }
 
