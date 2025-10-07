f3ca0dd02f9d34967880aa95afaba95044ba3753
pranayrauni <ipranaysingh@gmail.com>
2025-10-07 19:43:13 +0530

    create restaurant form

---
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,68 @@
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
+import MenuSection from "./MenuSection";
+import ImageSection from "./ImageSection";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  restaurantName: z.string().min(1, "Restaurant name is required"),
+  city: z.string().min(1, "City name is required"),
+  country: z.string().min(1, "Country name is required"),
+  deliveryPrice: z.coerce.number().min(1, "Delivery price is required"),
+  estimatedDeliveryTime: z.coerce
+    .number()
+    .min(1, "Estimated delivery time is required"),
+  cuisines: z.array(z.string()).nonempty("Please select at least one item"),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "Name is required"),
+      price: z.coerce.number().min(1, "Price is required"),
+    })
+  ),
+  imageFile: z.instanceof(File).describe("Image is required"),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema as any),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  const onSubmit = (formDataJson: restaurantFormData) => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
